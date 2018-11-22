1b5f647f01a6f8f4527be6072a26b4b9e8dc01db
iboskov <44433545+iboskov@users.noreply.github.com>
2018-11-22 16:46:20 +0100

    Update reset.py

--- libs/reset_device/reset.py
+++ libs/reset_device/reset.py
@@ -2,6 +2,7 @@
 import os
 import subprocess
 import reset_lib
+import socket
 
 #GPIO.setmode(GPIO.BCM)
 #GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -12,13 +13,21 @@ config_hash = reset_lib.config_file_hash()
 ssid_prefix = config_hash['ssid_prefix'] + " "
 hostapd_reset_required = reset_lib.hostapd_reset_check(ssid_prefix)
 
+def ipaddr():
+    try:
+        ip=[l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
+    except:
+        ip=""
+    return ip
+
 if hostapd_reset_required == True:
     reset_lib.update_hostapd(ssid_prefix)
     os.system('reboot')
-    
+
 wifi_conn=reset_lib.is_wifi_active()
+address=ipaddr()
 with open("/etc/resolv.conf") as file:
-    if 'nameserver 10.0.0.1' in file.read():
+    if 'nameserver 10.0.0.1' in file.read() and address != "":
         pass
     else:
         if wifi_conn == False:
