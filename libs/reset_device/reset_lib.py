a32912fa87000ddb3fbc364fd4a9d3f5746cb624
iboskov <44433545+iboskov@users.noreply.github.com>
2018-11-23 17:45:43 +0100

    Update reset_lib.py

--- libs/reset_device/reset_lib.py
+++ libs/reset_device/reset_lib.py
@@ -1,7 +1,7 @@
 import os
 import fileinput
 import subprocess
-import urllib.request
+import urllib.request, urllib.error
 
 def config_file_hash():
 	config_file = open('/etc/raspiwifi/raspiwifi.conf')
