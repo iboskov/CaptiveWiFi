3d5e5c92c49b74a31614707f97cbf2fc2c13265a
iboskov <44433545+iboskov@users.noreply.github.com>
2018-12-03 11:21:24 +0100

    Update app.py

--- libs/configuration_app/app.py
+++ libs/configuration_app/app.py
@@ -43,9 +43,6 @@ os.system('/etc/init.d/hostapd restart')
 
 @app.route('/login')
 def login():
-    # os.system('iwconfig wlan0 mode Managed')
-    # wifi_ap_array = scan_wifi_networks()
-    # os.system('/etc/init.d/hostapd restart')
     return render_template('app.html', wifi_ap_array=wifi_ap_array)
 
 
@@ -81,18 +78,6 @@ def save_credentials():
 
 ######## FUNCTIONS ##########
 
-# def scan_wifi_networks():
-# iwlist_raw = subprocess.Popen(['iwlist', 'scan'], stdout=subprocess.PIPE)
-# ap_list, err = iwlist_raw.communicate()
-# ap_array = []
-
-# for line in ap_list.decode('utf-8').rsplit('\n'):
-# if 'ESSID' in line:
-# ap_ssid = line[27:-1]
-# if ap_ssid != '':
-# ap_array.append(ap_ssid)
-
-# return ap_array
 
 def create_wpa_supplicant(ssid, wifi_key):
     temp_conf_file = open('interfaces', 'w')
@@ -117,25 +102,9 @@ def create_wpa_supplicant(ssid, wifi_key):
 
 def set_ap_client_mode():
     os.system('rm /etc/cron.raspiwifi/aphost_bootstrapper')
-    # os.system('cp /usr/lib/raspiwifi/reset_device/static_files/apclient_bootstrapper /etc/cron.raspiwifi/')
-    # os.system('chmod +x /etc/cron.raspiwifi/apclient_bootstrapper')
     os.system('mv /etc/dnsmasq.conf.original /etc/dnsmasq.conf')
     os.system('rm -rf /usr/lib/raspiwifi/APMODE')
-    # os.system('mv /etc/network/interfaces.original /etc/network/interfaces')
-    # os.system('cp /usr/lib/raspiwifi/reset_device/static_files/isc-dhcp-server.apclient /etc/default/isc-dhcp-server')
     os.system('reboot')
-
-
-#def config_file_hash():
-    #config_file = open('/etc/raspiwifi/raspiwifi.conf')
-    #config_hash = {}
-
-    #for line in config_file:
-        #line_key = line.split("=")[0]
-        #line_value = line.split("=")[1].rstrip()
-        #config_hash[line_key] = line_value
-
-    #return config_hash
 
 
 if __name__ == '__main__':
