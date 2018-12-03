005fcfbb167b668511005e5d8b9c84ba03e3f88b
iboskov <44433545+iboskov@users.noreply.github.com>
2018-12-03 11:38:20 +0100

    Update setup_lib.py

--- setup_lib.py
+++ setup_lib.py
@@ -35,27 +35,27 @@ def copy_configs():
 	
 	with fileinput.input("/etc/default/hostapd", inplace=True) as file:
 		for line in file:
-			print(line.replace('#DAEMON_CONF=""', 'DAEMON_CONF="/etc/hostapd/hostapd.conf"'), end='')
+			print(line.replace('#DAEMON_CONF=""', 'DAEMON_CONF="/etc/hostapd/hostapd.conf"'), end='') #change the file hostapd in /etc/default/ so that it knows the exact location of hostapd.conf
 		file.close()
 	
 		
-def checkid():
-	with open ('/etc/machine-id') as file:
-		line = file.readline().strip()
-		return line[-7:]
+def checkid(): #function to get the machine id
+	with open ('/etc/machine-id') as file: #open the file machine-id
+		line = file.readline().strip() #read only the first line of the file and strip the newline
+		return line[-7:] #return the last seven letters from the line
 
 		
-id = checkid()
+id = checkid() #save the output of checkid() as a string variable 
 	
 def update_main_config_file(entered_ssid, server_port_choice):
-	if entered_ssid != "":
+	if entered_ssid != "": #check if entered_ssid is empty, if so default APname is LGTC-AP
 		os.system('sed -i \'s/LGTC-AP/' + entered_ssid + '/\' /etc/raspiwifi/raspiwifi.conf')
 	else:
-		with fileinput.input("/etc/raspiwifi/raspiwifi.conf", inplace=True) as file:
-			for line in file:
-				print(line.replace('ssid_prefix=LGTC-AP', 'ssid_prefix=LGTC_' + id), end='')
-	if server_port_choice != "":
-		with fileinput.input("/etc/raspiwifi/raspiwifi.conf", inplace=True) as file:
-			for line in file:
-				print(line.replace('server_port=80', 'server_port=' + server_port_choice), end='')
+		with fileinput.input("/etc/raspiwifi/raspiwifi.conf", inplace=True) as file: #open the raspiwifi.conf file
+			for line in file: #read the file line by line
+				print(line.replace('ssid_prefix=LGTC-AP', 'ssid_prefix=LGTC_' + id), end='') #when the string ssid_prefix=LGTC-AP is found overwrite it with LGTC_ and the id of the machine, this is the new APname
+	if server_port_choice != "": #check if server_port_choice is empty, if so default port is 80
+		with fileinput.input("/etc/raspiwifi/raspiwifi.conf", inplace=True) as file: #open the raspiwifi.conf file
+			for line in file: #read the file line by line
+				print(line.replace('server_port=80', 'server_port=' + server_port_choice), end='') #when the string server_port=80 is found overwrite it with user's inputed port
 		#os.system('sed -i \'s/server_port=80/server_port=' + server_port_choice + '/\' /etc/raspiwifi/raspiwifi.conf')
