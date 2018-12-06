dd7c45bf46953d8962bd046bef7aaa21536a4d63
iboskov <44433545+iboskov@users.noreply.github.com>
2018-12-06 09:39:57 +0100

    Update initial_setup.py

--- initial_setup.py
+++ initial_setup.py
@@ -15,17 +15,18 @@ print("##### RaspiWiFi Intial Setup  #####")
 print("###################################")
 print()
 print()
-entered_ssid = input("Would you like to specify an SSID you'd like to use \nfor Host/Configuration mode? [default: LGTC-AP]: ")
+entered_ssid = ""
 print()
-server_port_choice = input("Which port would you like to use for the Configuration Page? [default: 80]: ")
+server_port_choice = ""
 print()
 print()
-install_ans = input("Are you ready to commit changes to the system? [y/N]: ")
+install_ans = "y"
 
 if(install_ans.lower() == 'y'):
 	setup_lib.install_prereqs()
 	setup_lib.copy_configs()
 	setup_lib.update_main_config_file(entered_ssid, server_port_choice)
+	os.system("touch /home/logatec/initial_boot")
 else:
 	print()
 	print()
@@ -49,7 +50,4 @@ print("#####################################")
 print()
 print()
 print("Initial setup is complete. A reboot is required to start in WiFi configuration mode...")
-reboot_ans = input("Would you like to do that now? [y/N]: ")
-
-if reboot_ans.lower() == 'y':
-	os.system('reboot')
+os.system('reboot')
