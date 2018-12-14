9eca130e8ce5b8c0b4c098410e84a5534f3f638c
iboskov <44433545+iboskov@users.noreply.github.com>
2018-12-14 11:15:19 +0100

    Update initial_setup.py

--- initial_setup.py
+++ initial_setup.py
@@ -8,46 +8,21 @@ if os.getuid():
 
 
 os.system('clear')
-print()
-print()
-print("###################################")
-print("##### RaspiWiFi Intial Setup  #####")
-print("###################################")
-print()
-print()
 entered_ssid = ""
 print()
 server_port_choice = ""
 print()
-print()
 install_ans = "y"
 
 if(install_ans.lower() == 'y'):
-	setup_lib.install_prereqs()
+	#setup_lib.install_prereqs()
 	setup_lib.copy_configs()
 	setup_lib.update_main_config_file(entered_ssid, server_port_choice)
 else:
-	print()
-	print()
-	print("===================================================")
-	print("---------------------------------------------------")
-	print()
-	print("RaspiWiFi installation cancelled. Nothing changed...")
-	print()
-	print("---------------------------------------------------")
-	print("===================================================")
-	print()
 	print()
 	sys.exit()
 
 os.system('clear')
 print()
-print()
-print("#####################################")
-print("##### RaspiWiFi Setup Complete  #####")
-print("#####################################")
-print()
-print()
-print("Initial setup is complete. A reboot is required to start in WiFi configuration mode...")
 os.system('rm -rf /home/logatec/initialboot')
 os.system('reboot')
