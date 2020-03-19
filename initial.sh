67bf6882e4c55c4a5f9eff853b51cbdd1da0ccee
E6 Ivan <ivan.boskov@live.com>
2020-03-19 20:42:36 +0100

    fix

--- initial.sh
+++ initial.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 FILE="/home/logatec/CaptiveWiFi/initialboot"
-
+apt-get install python3 python3-pip dnsmasq hostapd -y
+pip3 install flask
 if [ -e "$FILE" ];
 then
    ./initial_setup.py
